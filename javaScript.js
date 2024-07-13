13fcbc59052b15454489567e7887b6e773c4daf9
Mohamed Adam <mohameduopeople@gmail.com>
2024-07-13 04:02:44 +0300

    added the menu+ function and finshed from it.

---
+++ javaScript.js
@@ -0,0 +1,12 @@
+
+function moreMenu() {
+    event.preventDefault();
+    var menu = document.getElementById("menuButton"); // Use a variable to store the menu element
+    if (menu.style.display === "block") {
+      menu.style.display = "none"; // Hide the menu if it's currently displayed
+    } else {
+      menu.style.display = "block"; // Show the menu if it's currently hidden
+    }
+  }
+  
+  document.getElementById("moreColor").addEventListener("click", moreMenu);
